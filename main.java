5b960266ee65a672c46c144bcd9f7f3a23e48620
Seungjae Bae <bsjjay20060u14@gmail.com>
2026-05-13 18:26:03 +0900

    게임 시작하는 창

--- main.java
+++ main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
-        System.out.println("오목게임");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("아무 키나 입력하면 게임이 시작됩니다.");
+        scanner.nextLine();
+        System.out.println("게임 시작!");
+
+        
     }
 }
