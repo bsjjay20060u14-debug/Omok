0c0b77991e52501423ac3b5bb32cace43f3dc91d
Seungjae Bae <bsjjay20060u14@gmail.com>
2026-05-13 18:54:05 +0900

    흑돌 백돌 추가

--- main.java
+++ main.java
@@ -3,6 +3,9 @@ public class main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String whiteStone = " ○ ";
+        String blackStone = " ● ";
+
         System.out.println("아무 키나 입력하면 게임이 시작됩니다.");
         scanner.nextLine();
         System.out.println("게임 시작!");
