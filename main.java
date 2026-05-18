df338ed8846c3fb04a3e391e8a38808cd72627f7
Seyun Chong(정세윤) <seyun78521@gmail.com>
2026-05-18 19:54:26 +0900

    잘못된 좌표 입력시 메시지가 다음 보드가 출력된 이후에 출력되도록 수정

--- main.java
+++ main.java
@@ -4,6 +4,10 @@ import java.lang.ArrayIndexOutOfBoundsException;
 class PlaceStone{
     public static void stoneLocation(int[][] board){
         Scanner scanner = new Scanner(System.in);
+        if(main.e==1){
+                System.out.println("잘못된 좌표입니다. 다시 입력해주세요.");
+                main.e=0;
+            }
         if(main.n%2==1){//n이 홀수면 흑돌, 짝수면 백돌
             System.out.println("흑돌 차례입니다. x, y좌표를 입력해주세요.");
                 int x = scanner.nextInt();
@@ -34,6 +38,7 @@ class PlaceStone{
 }
 public class main {
     static int n=0;//static으로 선언하여 다른 클래스에서도 n값을 사용할 수 있도록 함
+    static int e=0;//에러 발생 시에 다시 보드 출력 후에 문장을 출력하기 위해 사용하는 변수
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String whiteStone = " ○ ";
@@ -74,14 +79,14 @@ public class main {
                 }
                 System.out.println();
             }
-            n++;
+            main.n++;
 
             try{
                 PlaceStone.stoneLocation(board);
             } 
             catch(ArrayIndexOutOfBoundsException e){
-                System.out.println("잘못된 좌표입니다. 다시 입력해주세요.");
-                n--;
+                main.e=1;
+                main.n--;
             }
         }
     }
