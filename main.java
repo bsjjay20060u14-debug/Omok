a0ccbffaf29d703914483cce37c715527aff75ba
Seyun Chong(정세윤) <seyun78521@gmail.com>
2026-05-18 21:24:13 +0900

    승리조건 구현_미완성

--- main.java
+++ main.java
@@ -1,8 +1,45 @@
 import java.util.Scanner;
 import java.lang.ArrayIndexOutOfBoundsException;
 
-class CountStone{
-    
+class CountStone {
+    public static boolean Winner(int[][] board, int y, int x, int stone) {
+        //각각 방향 세로, 가로, 우하향대각선, 좌하향대각선
+        int[][] directions = {{1, 0}, {0, 1}, {1, 1}, {1, -1}};
+
+        for (int[] d : directions) {
+            int count = 1; // 현재 돌을 포함해서 1로 시작
+
+            // 1. 한쪽(정방향)으로 최대 4칸 세기
+            for (int i = 1; i <= 4; i++) {
+                int ny = y + (d[0] * i);
+                int nx = x + (d[1] * i);
+
+                if (ny >= 0 && ny < 15 && nx >= 0 && nx < 15 && board[ny][nx] == stone) {
+                    count++;
+                } else {
+                    break; // 돌이 끊기면 즉시 중단
+                }
+            }
+
+            // 2. 반대쪽(역방향)으로 최대 4칸 세기 (d[0], d[1]에 마이너스만 붙임!)
+            for (int i = 1; i <= 4; i++) {
+                int ny = y + (-d[0] * i);
+                int nx = x + (-d[1] * i);
+
+                if (ny >= 0 && ny < 15 && nx >= 0 && nx < 15 && board[ny][nx] == stone) {
+                    count++;
+                } else {
+                    break; // 돌이 끊기면 즉시 중단
+                }
+            }
+
+            // 3. 정방향 + 역방향 + 내 돌을 다 합친 count가 5 이상이면 승리!
+            if (count >= 5) {
+                return true;
+            }
+        }
+        return false; 
+    }
 }
 
 class PrintBoard {
@@ -61,6 +98,10 @@ class PlaceStone{
 
             if(board[y][x]==0){
                 board[y][x]=1;
+                
+                main.lastX = x;//마지막 돌의 위치를 저장
+                main.lastY = y;
+                
             }
             else {
                 main.e=2;
@@ -75,6 +116,9 @@ class PlaceStone{
             
             if(board[y][x]==0){
                 board[y][x]=2;
+
+                main.lastX = x;//마지막 돌의 위치를 저장
+                main.lastY = y;
             }
             else {
                 main.e=2;
@@ -87,6 +131,9 @@ class PlaceStone{
 public class main {
     static int n=0;//static으로 선언하여 다른 클래스에서도 n값을 사용할 수 있도록 함
     static int e=0;//에러 발생 시에 다시 보드 출력 후에 문장을 출력하기 위해 사용하는 변수
+    
+    static int lastX = -1;//마지막 돌의 위치를 저장할 변수
+    static int lastY = -1;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -98,6 +145,18 @@ public class main {
         
         for(;;){ //보드 매턴마다 출력하기 위해 무한루프 추가함
             PrintBoard.printBoard(board);// 보드 출력을 PrintBoard 클래스로 객체화함
+
+            if (main.n>=9) {
+                if(CountStone.Winner(board, main.lastY, main.lastX, board[main.lastY][main.lastX])) {
+                if (main.n%2==0) {
+                        System.out.println("백돌 승리");
+                    } else {
+                        System.out.println("흑돌 승리");
+                    }
+                    break;
+                }
+            }
+
             main.n++;
 
             try{
